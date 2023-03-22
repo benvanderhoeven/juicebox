c6329160f47c03a1070c9d2f360d91cc09c7a9d7
Ben <benvanderhoeven@gmail.com>
2023-03-22 09:41:13 -0600

    second commit

--- db/index.js
+++ db/index.js
@@ -1,72 +1,86 @@
-// inside db/index.js
-const { Client } = require('pg'); // imports the pg module
 
-// supply the db name and location of the database
+// require('dotenv').config();
+const { Client } = require('pg');
+
+
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
-module.exports = {
-  client,
-}
-// inside db/index.js
+
 
 async function getAllUsers() {
-    const { rows } = await client.query(
-      `SELECT id, username 
+  const { rows } = await client.query(
+    `SELECT id, username, name, location, active
       FROM users;
     `);
-  
-    return rows;
+
+  return rows;
+}
+
+async function updateUser(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${key}"=$${index + 1}`
+  ).join(', ');
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
   }
-  
-  // and export them
-  module.exports = {
-    client,
-    getAllUsers,
-  }
 
-  // in db/index.js
-
-// async function createUser({ username, password }) {
-//   try {
-//     const result = await client.query(`
-
-//     `);
-
-//     return result
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-// in db/index.js
-
-// async function createUser({ username, password }) {
-//   try {
-//     const result = await client.query(`
-//       INSERT INTO users(username, password)
-//       VALUES ($1, $2);
-//     `, [username, password]);
-
-//     return result;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// later
-// module.exports = {
-  // add createUser here!
-// }
-async function createUser({ username, password }) {
   try {
-    const result = await client.query(`
-      INSERT INTO users(username, password) 
-      VALUES($1, $2) 
-      ON CONFLICT (username) DO NOTHING 
-      RETURNING *;
-    `, [username, password]);
+    const { rows: [user] } = await client.query(`
+        UPDATE users
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+      `, Object.values(fields));
 
-    return result;
+    return user;
   } catch (error) {
     throw error;
   }
+}
+
+
+
+async function createUser({
+  username,
+  password,
+  name,
+  location
+}) {
+  try {
+    const { rows: [user] } = await client.query(`
+        INSERT INTO users(username, password, name, location) 
+        VALUES($1, $2, $3, $4) 
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
+      `, [username, password, name, location]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+async function createPost({
+  authorId,
+  title,
+  content
+}) {
+  try {
+    const { rows: [post] } = await client.query(`
+      INSERT INTO posts( authorId, title, content ) 
+      VALUES($1, $2, $3,) 
+      RETURNING *;
+    `, [authorId, title, content]);
+
+    return post;
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  client, createUser, getAllUsers, updateUser,
 }
--- db/seed.js
+++ db/seed.js
@@ -1,128 +1,66 @@
-// inside db/seed.js
-
-// grab our client with destructuring from the export in index.js
-// const { client } = require('./index');
-
-// async function testDB() {
-//     try {
-// connect the client to the database, finally
-// client.connect();
-
-// queries are promises, so we can await them
-// const { rows } = await client.query(`SELECT * FROM users;`);
-
-// for now, logging is a fine way to see what's up
-//     console.log(rows);
-// } catch (error) {
-//     console.error(error);
-// } finally {
-// it's important to close out the client connection
-//         client.end();
-//     }
-// }
-
-// testDB();
-// inside db/seed.js
-
-// const {
-//     client,
-//     getAllUsers // new
-//   } = require('./index');
-
-//   async function testDB() {
-//     try {
-//       client.connect();
-
-//       const users = await getAllUsers();
-//       console.log(users);
-//     } catch (error) {
-//       console.error(error);
-//     } finally {
-//       client.end();
-//     }
-//   }
-
-// inside db/seed.js
-
-// this function should call a query which drops all tables from our database
-// async function dropTables() {
-//     try {
-//       await client.query(`
-
-//       `);
-//     } catch (error) {
-//       throw error; // we pass the error up to the function that calls dropTables
-//     }
-//   }
-
-//   // this function should call a query which creates all tables for our database 
-//   async function createTables() {
-//     try {
-//       await client.query(`
-
-//       `);
-//     } catch (error) {
-//       throw error; // we pass the error up to the function that calls createTables
-//     }
-//   }
-
-//   async function rebuildDB() {
-//     try {
-//       client.connect();
-
-//       await dropTables();
-//       await createTables();
-//     } catch (error) {
-//       console.error(error);
-//     } finally {
-//       client.end();
-//     }
-//   }
-
-//   rebuildDB();
-
-// in db/seed.js
-
-// async function dropTables() {
-//   try {
-//     await client.query(`
-//       DROP TABLE IF EXISTS users;
-//     `);
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// in db/seed.js
-
-// async function createTables() {
-//   try {
-//     await client.query(`
-//       CREATE TABLE users (
-//         id SERIAL PRIMARY KEY,
-//         username varchar(255) UNIQUE NOT NULL,
-//         password varchar(255) NOT NULL
-//       );
-//     `);
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// db/seed.js
 
 const {
-  // other imports,
-  createUser
+  
+  createUser, client, getAllUsers, updateUser,
 } = require('./index');
+
+
+async function dropTables() {
+  try {
+    await client.query(`
+      DROP TABLE IF EXISTS posts;
+    `);
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+//cannot have last coma in a SQL query
+
+async function createTables() {
+  try {
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT true 
+      );
+    `);
+    await client.query(`
+    CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    "authorId" INTEGER REFERENCES users(id) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    active BOOLEAN DEFAULT true
+    );
+    `)
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+
+
 
 // new function, should attempt to create a few users
 async function createInitialUsers() {
   try {
     console.log("Starting to create users...");
 
-    const albert = await createUser({ username: 'albert', password: 'bertie99' });
-    const albertTwo = await createUser({ username: 'albert', password: 'imposter_albert' });
+    const albert = await createUser({ username: 'albert', password: 'bertie99', location: 'Santa Cruz', name: 'Al Bert' });
+    const sandra = await createUser({ username: 'sandra', password: '2sandy4me', location: 'HELL!', name: 'Just Sandra' });
+    const glamgal = await createUser({ username: 'glamgal', password: 'soglam', location: 'Chicago', name: 'Joshua' });
+
+    // { id: 2, username: 'sandra', password: '2sandy4me' },
+    // { id: 3, username: 'glamgal', password: 'soglam' }
 
     console.log(albert);
 
@@ -146,77 +84,31 @@ async function rebuildDB() {
   }
 }
 
-// old stuff below here! 
-
-// db/seed.js
-
-// const {
-//   client,
-//   getAllUsers
-// } = require('./index');
-
-// async function dropTables() {
-//   try {
-//     console.log("Starting to drop tables...");
-
-//     await client.query(`
-//       DROP TABLE IF EXISTS users;
-//     `);
-
-//     console.log("Finished dropping tables!");
-//   } catch (error) {
-//     console.error("Error dropping tables!");
-//     throw error;
-//   }
-// }
-
-// async function createTables() {
-//   try {
-//     console.log("Starting to build tables...");
-
-//     await client.query(`
-//       CREATE TABLE users (
-//         id SERIAL PRIMARY KEY,
-//         username varchar(255) UNIQUE NOT NULL,
-//         password varchar(255) NOT NULL
-//       );
-//     `);
-
-//     console.log("Finished building tables!");
-//   } catch (error) {
-//     console.error("Error building tables!");
-//     throw error;
-//   }
-// }
-
-// async function rebuildDB() {
-//   try {
-//     client.connect();
-
-//     await dropTables();
-//     await createTables();
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// async function testDB() {
-//   try {
-//     console.log("Starting to test database...");
-
-//     const users = await getAllUsers();
-//     console.log("getAllUsers:", users);
-
-//     console.log("Finished database tests!");
-//   } catch (error) {
-//     console.error("Error testing database!");
-//     throw error;
-//   }
-// }
 
 
-// rebuildDB()
-//   .then(testDB)
-//   .catch(console.error)
-//   .finally(() => client.end());
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    const users = await getAllUsers();
+    console.log("getAllUsers:", users);
+    console.log("Calling updateUser on users[0]")
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY"
+    });
+    console.log("Result:", updateUserResult);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
+  }
+}
+
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
 
